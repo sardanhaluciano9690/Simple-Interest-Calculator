655d018f2be236b42ed751ff49d4e120c460a8fc
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-05-25 10:43:12 -0300

    script ready

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
+// }
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+
+
+var p,t,r,si,ci;
+p = document.getElementById ("first").value;
+t = document.getElementById ("second").value;
+r = document.getElementById ("third").value;
+si = parseInt((p*t*r)/100 );
+amount = p*Math.pow((1 +r/100),t );
+ci = amount-p;
+document.getElementById ('num').innerHTML ="Simple Interest : "+si;
+// document.getElementById ('num1').innerHTML ="Compound Interest : "+ci;
 }
-        
+

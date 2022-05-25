d65933f20ddf49d46948fb12cf0d86e5464daac8
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-05-25 11:43:51 -0300

    script ready before final layout

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ function compute()
 
 
 
-var p,t,r,si,ci;
+var p,t,r,si,ci = Number;
 p = document.getElementById ("first").value;
 t = document.getElementById ("second").value;
 r = document.getElementById ("third").value;
 si = parseInt((p*t*r)/100 );
 amount = p*Math.pow((1 +r/100),t );
 ci = amount-p;
-document.getElementById ('num').innerHTML ="Simple Interest : "+si;
+const dt = new Date();
+let thisYear  = dt.getFullYear();
+let endYear = parseInt(t) + parseInt(thisYear)  ;
+
+
+document.getElementById ('num1').innerHTML ="If you deposit: "+p +", ";
+document.getElementById ('num2').innerHTML =" "+" at an interest rate of "+r + "%.";
+document.getElementById ('num3').innerHTML ="You will receive an amount of: "+si +",";
+document.getElementById ('num4').innerHTML ="in the year of: "+ endYear+".";
 // document.getElementById ('num1').innerHTML ="Compound Interest : "+ci;
 }
 

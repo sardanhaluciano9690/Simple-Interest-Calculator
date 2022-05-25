f7f27951a05ef8694fc21d3276190d921889a756
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-05-25 15:20:17 -0300

    Final result with alerts2

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ var p,t,r,si,ci = Number;
 p = document.getElementById ("principal").value;
 if (p <= 0){
     alert("Please enter a positive value");
+    
 }
 
 t = document.getElementById ("years").value;
 r = document.getElementById ("rate").value;
-if (r <= 0){
-    alert('Choose a number greater than zero');
-}
+// if (r <= 0){
+//     alert('Choose a number greater than zero');
+// }
 si = parseInt((p*t*r)/100 );
 amount = p*Math.pow((1 +r/100),t );
 ci = amount-p;
@@ -30,4 +31,9 @@ document.getElementById ('num4').innerHTML ="in the year of: "+ endYear;
 // document.getElementById ('num1').innerHTML ="Compound Interest : "+ci;
 }
 
-// after comminting
+// highlightText 
+
+function changeTextHighlight()
+{
+    document.getElementById(num1,num2, num3, num4).innerHTML.replace
+}
